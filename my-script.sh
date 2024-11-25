ff02dce0302ebd86a4fdf0c01dfdabc20d50f6da
afristorm-3 <afristorm3@gmail.com>
2024-11-25 04:27:15 -0500

    new shell

---
+++ my-script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# echo without double quote
+echo Hello World
+echo learning new thing
+echo A good start
+# This is a comment!
+echo Hello       Word
+echo HelloWorld
+echo Hello * World
+echo Hello "	" World
+
+# echo with double quote
+
+echo "Hello    World"
+echo "Hello World"
+echo "Hello * World"
+
